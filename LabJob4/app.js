26471da3bd0b86cd9e8aa18b2e08e4861e6e4119
YanBorka2 <borka.yan@student.uzhnu.edu.ua>
2024-10-20 00:36:12 +0300

    four commit

---
+++ LabJob4/app.js
@@ -0,0 +1,7 @@
+function displayResult() {
+    document.getElementById("messenge-inf").value = null;
+    document.getElementById("name-inf").value = null;
+    document.getElementById("email-inf").value = null;
+    document.getElementById("date-inf").value = null;
+}
+  
